7d153ce8096a6622aa8c549d82975e7a05e56dba
KKH1992 <goldhyuk92@gmail.com>
2023-11-11 12:57:39 +0900

    fest: 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-between h-[50px]  items-center ">
+      
+      <div className="px-5">
+        <div>kakao i connect Center</div>
+      </div>
+      
+      <div className="flex gap-5 text-green-300">
+        <div>주요 기능</div>
+        <div>활용 사례</div>
+        <div>요금제</div>
+        <div>고객 지원</div>
+      </div>
+      
+      <div className="flex gap-10 px-5 ">
+        <div className="bg-white px-5 py-2">로그인</div>
+        <div className="rounded-full hover:brightness-90 bg-yellow-500 px-5 py-2">구독하기</div>
+        <div className="rounded-full text-white bg-black px-5 py-2">consloe </div>
+      </div>
     </div>
   );
 }
