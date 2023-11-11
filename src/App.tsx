ccc43f7e1fc4a7d4378ba8a1abc7108507c8c41a
Lunaluna <momokarenv@gmail.com>
2023-11-11 12:57:49 +0900

    feat:헤더추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,32 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-between height h-[87px] items-center font-semibold px-20">
+      <div>
+        <div>Kakao i connect center</div>
+      </div>
+
+      <div className="flex justify-between items-center gap-8 text-gray-500">
+       <div  className="py-3">주요 기능</div>
+       <div  className="py-3">활용 사례</div>
+       <div  className="py-3">요금제</div>
+       <div  className="py-3">고객 지원</div>
+      </div>
+  
+
+      <div className="flex justify-beween items-center gap-3">
+        <div className="py-3 px-5 rounded-full">로그인</div>
+        <div className="bg-gray-200 h-5 w-0.5"></div>
+        <div className="bg-yellow-400 py-3 px-5 rounded-full">구독하기</div>
+        <div className="text-white bg-black py-3 px-5 rounded-full">console</div>
     </div>
+
+    <div>
+      c kakao enterpirse corp.
+
+    </div>
+</div>
+
   );
 }
 
