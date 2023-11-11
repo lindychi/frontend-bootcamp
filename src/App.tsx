e69c12b4cf9216ff46d35dfafdcc71d0f06160bf
hanchi <hanchi@tosky.co.kr>
2023-11-11 13:01:47 +0900

    feat: 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full flex justify-between h-[87px] bg-slate-300 items-center font-semibold scroll-px-40">
+      <div>kakao i connect center</div>
+      <div className="flex gap-2 text-neutral-500">
+        <div className="hover:text-neutral-950">주요 기능</div>
+        <div className="hover:text-neutral-950">활용 사례</div>
+        <div className="hover:text-neutral-950">요금제</div>
+        <div className="hover:text-neutral-950">고객 지원</div>
+      </div>
+      <div className="flex items-center gap-2">
+        <div>로그인</div>
+        <div className="hover:brightness-90 bg-yellow-400 px-3 py-1 rounded-full">
+          구독하기
+        </div>
+        <div className="text-white bg-black px-3 py-1 rounded-full">
+          Console
+        </div>
+      </div>
     </div>
   );
 }
