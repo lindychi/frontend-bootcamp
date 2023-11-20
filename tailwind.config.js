0980729e1378eb4af48e05c92727a2021c314633
hanchi <hanchi@tosky.co.kr>
2023-11-20 12:39:44 +0900

    feat: 색상 미리 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          100: "#D5CCFF",
+          800: "#2B1887",
+        },
+        priority: {
+          high: "#E42C5F",
+          medium: "#ECB800",
+          low: "#2D41A7",
+          inactive: "#D9D9D9",
+        },
+      },
+    },
   },
   plugins: [],
 };
