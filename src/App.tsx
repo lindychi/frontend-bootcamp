b8ff5111cec9aebe9c93a77e6a2751c42b251db8
KKH1992 <goldhyuk92@gmail.com>
2023-11-25 16:07:43 +0900

    25일 수업중간

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { todoData } from "./consts/todoList";
+import { todoData as originTodoData } from "./consts/todoList";
 import { HighlightSpanKind } from "typescript";
 
 function App() {
+  const [todoData, setTodoData] = useState(originTodoData);
   const todoList = todoData.filter((item) => item.progress === "TODO");
   const doneList = todoData.filter((item) => item.progress === "DONE");
-
+  const [value, setValue] = useState("");
   return (
-    <div className="w-screen h-screen justify-center items-center bg-blue-800">
+    <div className="w-screen h-full items-center bg-blue-800">
       <div>
         <div className="felx flex-row ">
-          <div className="flex flex-row justify-center items-center gap-10 py-20">
+          <div className="p-4 bg-blue-100 rounded">
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                setTodoData([
+                  ...todoData,
+                  {
+                    title: value,
+                    progress: "TODO",
+                    level: 2,
+                    priority: "high",
+                    dueDate: "Mon",
+                    author: "한치",
+                  },
+                ]);
+                setValue("");
+              }}
+            >
+              추가
+            </button>
+          </div>
+          <div className="h-fit flex flex-row justify-center gap-10 py-20">
             <div className="bg-blue-100 rounded-xl p-6">
-              <div className="w-[500px] h-[1000px] flex flex-col gap-6">
+              <div className="w-[500px] h-fit flex flex-col gap-6">
                 <div className="flex gap-4 text-4xl text-blue-800">
-                  <svg
-                    width="40"
-                    height="40"
-                    viewBox="0 0 40 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M26.1096 6.47981C25.6652 4.90453 24.2174 3.75 22.5 3.75H17.5C15.7826 3.75 14.3348 4.90453 13.8904 6.47981M26.1096 6.47981C26.2011 6.80417 26.25 7.14637 26.25 7.5V7.5C26.25 8.19036 25.6904 8.75 25 8.75H15C14.3096 8.75 13.75 8.19036 13.75 7.5V7.5C13.75 7.14637 13.7989 6.80417 13.8904 6.47981M26.1096 6.47981C27.1865 6.56114 28.2576 6.66379 29.3222 6.78735C31.1565 7.00023 32.5 8.58167 32.5 10.4283V32.5C32.5 34.5711 30.8211 36.25 28.75 36.25H11.25C9.17893 36.25 7.5 34.5711 7.5 32.5V10.4283C7.5 8.58167 8.84347 7.00023 10.6778 6.78735C11.7424 6.66379 12.8135 6.56114 13.8904 6.47981"
-                      stroke="#2B1887"
-                      strokeWidth="2.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
+                  <TodoIcon />
                   To-Do
                 </div>
 
@@ -71,6 +83,7 @@ function App() {
                         >
                           <path
                             d="M0.333252 6.5H20.3333C22.5424 6.5 24.3333 8.29086 24.3333 10.5V15.0714C24.3333 16.965 22.7982 18.5 20.9047 18.5H12.3333C5.70584 18.5 0.333252 13.1274 0.333252 6.5Z"
+                            className="bg-priority-{item.priority}"
                             fill="item.priority"
                           />
                         </svg>
@@ -129,24 +142,10 @@ function App() {
               </div>
             </div>
 
-            <div className="bg-blue-100 rounded-xl p-6">
-              <div className="w-[500px] h-[900px] flex flex-col gap-6">
+            <div className="bg-blue-100 rounded-xl p-6 h-fit">
+              <div className="w-[500px] h-fit flex flex-col gap-6">
                 <div className="flex gap-4 text-4xl text-blue-800">
-                  <svg
-                    width="41"
-                    height="40"
-                    viewBox="0 0 41 40"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M17.375 3.75H9.875C8.83947 3.75 8 4.58947 8 5.625V34.375C8 35.4105 8.83947 36.25 9.875 36.25H31.125C32.1605 36.25 33 35.4105 33 34.375V19.375M17.375 3.75H18C26.2843 3.75 33 10.4657 33 18.75V19.375M17.375 3.75C20.4816 3.75 23 6.2684 23 9.375V11.875C23 12.9105 23.8395 13.75 24.875 13.75H27.375C30.4816 13.75 33 16.2684 33 19.375M15.5 25L19.25 28.75L25.5 20"
-                      stroke="#2B1887"
-                      strokeWidth="2.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
+                  <DoneIcon />
                   Done
                 </div>
                 {doneList.map((item, index) => (
