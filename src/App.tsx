3c9a2e041cec54f6dd8b143cd2779f77ce05afed
hanchi <hanchi@tosky.co.kr>
2023-11-28 22:00:44 +0900

    fix: 베이스 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import clsx from "clsx";
 import "./App.css";
 
+import { dayList } from "./consts/calendar";
+
+import { getCalendarDates, getMonthString } from "./libs/calendar";
+
+import Arrow from "./icons/Arrow";
+import Hamburger from "./icons/Hamburger";
+import Search from "./icons/Search";
+import Plus from "./icons/Plus";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [selectedMonth, setSelectedMonth] = React.useState(12);
+  const [selectedYear, setSelectedYear] = React.useState(2023);
+  const targetCalendarDates = getCalendarDates(selectedYear, selectedMonth);
+
+  return <div></div>;
 }
 
 export default App;
