fe14171e9988bc9d57657adab0cc72e13303473e
Lunalunav <momokarenv@gmail.com>
2023-11-30 22:04:08 +0900

    기초 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,44 @@ function App() {
     selectedYear,
     selectedMonth
   );
+  
 
-  return <div></div>;
+  return (
+    <div className="calendar flex justify-start w-[1465px]  rounded-md h-auto border solid rgba(157, 158, 159, 0.60)">
+      <div className="sidebar flex-col w-[250px] border solid rgba(157, 158, 159, 0.60)">
+        <div className="month_s flex-col w-[250px] h-[47px] py-[10px] px-[16px]">JANUDXKdlka</div>
+        <div className="calendar_s w-[250px] h-[218px] p-[10px]  border solid rgba(157, 158, 159, 0.60)">2</div>
+        <div className="schedule w-[250px] h-[409px] py-[10px] px-[16px] gap-[10px]">3</div>
+      </div>
+
+      <div className="content">
+        <div className="content_top flex w-[1214px] h-[77px] p-[16px] justify-between border solid rgba(157, 158, 159, 0.60)">
+          <div className="leftcolumn flex justify-between">
+            <div className="Monthtitle">Jan</div>
+              <div>< Hamburger /></div>            
+              <div>month</div>
+          </div>     
+          <div className="rightcolumn flex justify-between gap-[16px]">
+            <div>돋보기</div>
+            <div>Add event</div>
+          </div>
+        </div>  
+          
+
+        <div className="weeks flex justify-between">
+          <p>sun</p>
+          <p>sun</p>
+          <p>sun</p>
+          <p>sun</p>
+          <p>sun</p>
+          <p>sun</p>
+          <p>sun</p>
+          
+        </div>
+            <div className="days">일</div>
+      </div>    
+    </div>
+  )
 }
 
 export default App;
