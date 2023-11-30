9c387c232e270b79367d6d2305d768d794bf6882
Lunalunav <momokarenv@gmail.com>
2023-11-30 23:46:35 +0900

    header완료

--- src/App.tsx
+++ src/App.tsx
@@ -23,36 +23,43 @@ function App() {
   return (
     <div className="calendar flex justify-start w-[1465px]  rounded-md h-auto border solid rgba(157, 158, 159, 0.60)">
       <div className="sidebar flex-col w-[250px] border solid rgba(157, 158, 159, 0.60)">
-        <div className="month_s flex-col w-[250px] h-[47px] py-[10px] px-[16px]">JANUDXKdlka</div>
+        <div className="month_s flex-col w-[250px] h-[47px] py-[10px] px-[16px] font-medium">December</div>
         <div className="calendar_s w-[250px] h-[218px] p-[10px]  border solid rgba(157, 158, 159, 0.60)">2</div>
         <div className="schedule w-[250px] h-[409px] py-[10px] px-[16px] gap-[10px]">3</div>
       </div>
 
       <div className="content">
         <div className="content_top flex w-[1214px] h-[77px] p-[16px] justify-between border solid rgba(157, 158, 159, 0.60)">
-          <div className="leftcolumn flex justify-between">
-            <div className="Monthtitle">Jan</div>
-              <div>< Hamburger /></div>            
-              <div>month</div>
+          <div className="leftcontent flex justify-between gap-4 items-center">
+            <div>< Hamburger /></div>      
+            <div className="Monthtitle flex gap-[4px] items-center">
+              <div className="text-3xl text-center font-semibold">December</div>
+              <div className="text-3xl text-center">2023</div>
+            </div>
+            <div className="flex gap-[4px] w-[75px] h-[35px] border solid border-primary text-primary rounded p-[8px] items-center text-sm">Month <Arrow /></div>
+            
           </div>     
-          <div className="rightcolumn flex justify-between gap-[16px]">
-            <div>돋보기</div>
-            <div>Add event</div>
+          <div className="rightcontent flex justify-between gap-[16px] items-center">
+            <div><Search /></div>
+            <div className="flex w-auto h-[35px] p-[8px] rounded bg-primary text-white text-sm gap-[4px] items-center">Add event <Plus /> </div>
           </div>
         </div>  
           
 
-        <div className="weeks flex justify-between">
-          <p>sun</p>
-          <p>sun</p>
-          <p>sun</p>
-          <p>sun</p>
-          <p>sun</p>
-          <p>sun</p>
-          <p>sun</p>
-          
+        <div className="weeks flex items-center justify-between w-100% h-[24px] px-[10px] ">
+        {/* mx-auto는 수평 여백을 자동으로 설정하여 내용을 가운데로 이동시키는 클래스입니다.  */}
+          {}
+
+          <p className="day mx-auto text-center">sun</p>
+          <p className="day mx-auto text-center">sun</p>
+          <p className="day mx-auto text-center">sun</p>
+          <p className="day mx-auto text-center">sun</p>
+          <p className="day mx-auto text-center">sun</p>
+          <p className="day mx-auto text-center">sun</p>
         </div>
-            <div className="days">일</div>
+            <div className="daysbox w-100% h-[184.60px] py-[3px] px-[3px] border solid rgba(157, 158, 159, 0.60)">
+              
+            </div>
       </div>    
     </div>
   )
