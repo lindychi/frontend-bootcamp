4a0a42775b45f751cd67bcfd7bd74f68cdd1be10
swinglala <wishmin82@naver.com>
2023-12-01 16:16:26 +0900

    날짜 두자리수

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import Hamburger from "./icons/Hamburger";
 import Search from "./icons/Search";
 import Plus from "./icons/Plus";
 
+
+
 function App() {
   const today = new Date();
   const [selectedMonth, setSelectedMonth] = React.useState(today.getMonth() + 1);
@@ -31,18 +33,18 @@ function App() {
                 date.getDate() === today.getDate() &&
                 date.getMonth() === today.getMonth() &&
                 date.getFullYear() === today.getFullYear();
-              
+               
               
               return (
                 <div
                 key={index}
-                className={clsx('w-[30px] h-[33px]', {
-                  'bg-primary text-white': isToday,
+                className={clsx('w-[30px] h-[33px] grid justify-center items-center', {
+                  'bg-primary text-white rounded-full': isToday,
                   'text-black': !isToday && isCurrentMonth,
                   'text-zinc-500': !isToday && !isCurrentMonth,
                 })}
                 >
-                  {date.getDate()}
+                  {date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`}
                 </div>
               );
             })}
@@ -54,24 +56,27 @@ function App() {
    
 
 
-<div className="w-[1214px] outer-box py-3">
-   <div className="flex flex-row place-content-between items-center"> 
-     <div className="flex flex-row items-center ">
+<div className="w-[1214px] outer-box ">
+   <div className="flex flex-row place-content-between items-center p-4"> 
+     <div className="flex flex-row items-center gap-4 ">
       <div>< Hamburger /></div>
-      <div className="text-2xl font-bold">{getMonthString(12)} </div>
-      <div className="text-2xl"> {selectedYear}</div>
-      <select></select>
+      <div className="text-[30px] font-bold">{getMonthString(12)} </div>
+      <div className="text-[30px]"> {selectedYear}</div>
+      <div className="flex flex-row border-primary  border-solid border-[1px]  p-2 gap-1 rounded">
+        <div>Month</div>
+        <div><Arrow /></div>
+      </div>
      </div>
-     <div className="flex flex-row items-center"> 
+     <div className="flex flex-row items-center gap-4"> 
       <div><Search /></div>
-      <div className="flex flex-row bg-primary items-center text-white p-2">
+      <div className="flex flex-row bg-primary items-center text-white p-2 gap-2 rounded">
         <div>Add event</div>
         <div> <Plus/></div>
         </div>
 
      </div>
     </div> 
-   <div className="grid grid-cols-7 outer-box"> {dayList.map((day, index) => (<div key={index}>{day.medium} </div>))} </div>  
+   <div className="grid grid-cols-7  outer-box "> {dayList.map((day, index) => (<div className="grid justify-center" key={index}>{day.medium} </div>))} </div>  
    <div className="grid grid-cols-7 w-[1214px] h-min-[923px]">{targetCalendarDates.map((date: Date, index) => {
               const isCurrentMonth =
                 date.getMonth() + 1 === selectedMonth && date.getFullYear() === selectedYear;
@@ -90,7 +95,7 @@ function App() {
                   'text-zinc-500 bg-zinc-100 opacity-30': !isToday && !isCurrentMonth,
                 })}
                 >
-                  {date.getDate()}
+                  {date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`}
                 </div>
               );
             })}
