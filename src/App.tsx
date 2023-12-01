5994d29b339fe97a48377b10c44943f43dea4ab1
Lunalunav <momokarenv@gmail.com>
2023-12-01 22:21:14 +0900

    date삽입

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="calendar flex justify-start w-[1465px]  rounded-md h-auto border solid rgba(157, 158, 159, 0.60)">
       <div className="sidebar flex-col w-[250px] border solid rgba(157, 158, 159, 0.60)">
-        <div className="month_s flex-col w-[250px] h-[47px] py-[10px] px-[16px] font-medium">December</div>
+        <div className="month_s flex-col w-[250px] h-[47px] py-[10px] px-[16px] font-medium">{getMonthString(12)}</div>
         <div className="calendar_s w-[250px] h-[218px] p-[10px]  border solid rgba(157, 158, 159, 0.60)">2</div>
         <div className="schedule w-[250px] h-[409px] py-[10px] px-[16px] gap-[10px]">3</div>
       </div>
@@ -33,7 +33,7 @@ function App() {
           <div className="leftcontent flex justify-between gap-4 items-center">
             <div>< Hamburger /></div>      
             <div className="Monthtitle flex gap-[4px] items-center">
-              <div className="text-3xl text-center font-semibold">December</div>
+              <div className="text-3xl text-center font-semibold">{getMonthString(12)}</div>
               <div className="text-3xl text-center">2023</div>
             </div>
             <div className="flex gap-[4px] w-[75px] h-[35px] border solid border-primary text-primary rounded p-[8px] items-center text-sm">Month <Arrow /></div>
@@ -46,20 +46,20 @@ function App() {
         </div>  
           
 
-        <div className="weeks flex items-center justify-between w-100% h-[24px] px-[10px] ">
-        {/* mx-auto는 수평 여백을 자동으로 설정하여 내용을 가운데로 이동시키는 클래스입니다.  */}
-          {}
 
-          <p className="day mx-auto text-center">sun</p>
-          <p className="day mx-auto text-center">sun</p>
-          <p className="day mx-auto text-center">sun</p>
-          <p className="day mx-auto text-center">sun</p>
-          <p className="day mx-auto text-center">sun</p>
-          <p className="day mx-auto text-center">sun</p>
+
+        <div className="day flex items-center justify-between w-100% h-[24px]">
+          {dayList.map(({medium},index) => ( 
+            <div key={index} className="day px-[10px] mx-auto text-center text-xs">{medium}</div>         
+              /* mx-auto는 수평 여백을 자동으로 설정하여 내용을 가운데로 이동시키는 클래스입니다.  */
+          ))}
         </div>
-            <div className="daysbox w-100% h-[184.60px] py-[3px] px-[3px] border solid rgba(157, 158, 159, 0.60)">
-              
-            </div>
+
+
+
+          <div className="datebox grid grid-cols-7 w-100% h-100% py-[3px] px-[4px] border solid rgba(157, 158, 159, 0.60)">
+            {targetCalendarDates.map((date: Date) => (<div className="date  w-[30px] h-[33px] p-[4px] font-medium text-xs">{date.getDate()}</div>))}
+          </div>
       </div>    
     </div>
   )
