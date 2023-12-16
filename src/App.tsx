407e0413f96621ccf8390a7c79ce27459386e819
Lunalunav <momokarenv@gmail.com>
2023-12-16 11:59:40 +0900

    done완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import clsx from "clsx";
 import "./App.css";
 import {
   Date,
@@ -77,16 +78,40 @@ function App() {
               >
                 <div className="frame1 styled-title pb-8">{item.title}</div>
                 <div className="frame2-1 flex w=[156px] space-x-3 items-center ">
-                  {/* <div className={`${priorityMap[item.priority] || "default"} styled-Date p-2 rounded-lg`}>{item.dueDate}</div>  */}
+                  <div
+                    className={`${
+                      priorityMap[item.priority] || "default"
+                    } styled-Date p-2 rounded-lg`}
+                  >
+                    {item.dueDate}
+                  </div>
                   <div className="frame2-2 flex gap-2">
-                    <div className="Priority">
-                      <MySVGIcon />
-                    </div>
-                    <div className="Priority">
-                      <MySVGIcon />
-                    </div>
-                    <div className="Priority">
-                      <MySVGIcon />
+                    <div className="frame2-2 flex gap-2">
+                      {[1, 2, 3].map((currentLevel) => (
+                        <div style={{ width: 24, height: 24 }}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 55 55"
+                            fill="none"
+                            width="100%"
+                            height="100%"
+                          >
+                            <path
+                              d="M0.737061 14.2498H45.856C50.8397 14.2498 54.8797 18.2898 54.8797 23.2735V33.5864C54.8797 37.8582 51.4168 41.3211 47.1451 41.3211H27.8084C12.8573 41.3211 0.737061 29.2008 0.737061 14.2498Z"
+                              // fill="#D9D9D9"
+                              className={clsx([
+                                {
+                                  "!fill-[#D9D9D9]": item.level < currentLevel,
+                                  "fill-[#2D41A7]": item.priority === "low",
+                                  "fill-[#ECB800]": item.priority === "medium",
+                                  "fill-[#E42C5F]": item.priority === "high",
+                                  // item = map에 있는 imte 용어와 일치해야한다ㅣ.
+                                },
+                              ])}
+                            />
+                          </svg>
+                        </div>
+                      ))}
                     </div>
                   </div>
                   <div className="frame2-3 styled-name w-[291.5px] ">
