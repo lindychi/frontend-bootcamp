4a2ca1ec42831ed688f4f4f502a0dbe3bad090c6
swinglala <wishmin82@naver.com>
2023-12-21 17:35:44 +0900

    add time

--- src/components/ToDoAdd.tsx
+++ src/components/ToDoAdd.tsx
@@ -13,10 +13,11 @@ type ToDoAddProps = {
 const ToDoAdd: React.FC<ToDoAddProps> = ({ setTodoData }) => {
   const [title, setTitle] = useState('');
   const [selectedDate, setDate] = useState<Date | null>(null);
+  const [selectedTime, setTime] = useState<string>('08:00');
 
   const handleAddTodo = () => {
-    if (title && selectedDate) {
-      const newTodo = { title, time: '8:00', date: selectedDate as Date };
+    if (title && selectedDate && selectedTime) {
+      const newTodo = { title, time: selectedTime, date: selectedDate as Date };
 
       // 이 부분에서 로컬 스토리지에 데이터 저장
       const existingTodos = JSON.parse(localStorage.getItem('todos') || '[]');
@@ -28,11 +29,12 @@ const ToDoAdd: React.FC<ToDoAddProps> = ({ setTodoData }) => {
 
       setTitle('');
       setDate(null);
+      setTime('08:00');
     }
   };
 
   return (
-    <div>
+    <div className='flex flex-col'>
       Title{''}
       <input
         type="text"
@@ -47,6 +49,12 @@ const ToDoAdd: React.FC<ToDoAddProps> = ({ setTodoData }) => {
          const selectedDateValue = event.target.value;
          setDate(new Date(selectedDateValue));
         }}
+      />
+      Time{' '}
+      <input
+        type="time"
+        value={selectedTime}
+        onChange={(event) => setTime(event.target.value)}
       />
       <button
         className="bg-primary text-white"
