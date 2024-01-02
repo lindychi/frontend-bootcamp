a2c8886e1fc4fb895f60d8350b759f71659c6fa2
swinglala <wishmin82@naver.com>
2024-01-02 18:51:50 +0900

    이게아닌데

--- src/components/DayView.tsx
+++ src/components/DayView.tsx
@@ -32,10 +32,30 @@ const filterDate = (todos: Todo[], selectedDate: Date | null, selectedHour: numb
       todoDate.getDate() === selectedDate.getDate() &&
       todoDate.getMonth() === selectedDate.getMonth() &&
       todoDate.getFullYear() === selectedDate.getFullYear() &&
-      todo.startTime && // Check if startTime is defined
+      todo.startTime && 
       Number(todo.startTime.split(":")[0]) === selectedHour      
     );
   });
+};
+
+const calculateTopPosition = (startTime: string): string => {
+  const [startHour, startMinute] = startTime.split(":").map(Number);
+  const totalMinutes = startHour * 60 + startMinute;
+  return `${totalMinutes / 60 * 80}px`;
+};
+
+const calculateHeight = (startTime: string, endTime: string): string => {
+  const [startHour, startMinute] = startTime.split(":").map(Number);
+  const [endHour, endMinute] = endTime.split(":").map(Number);
+  const totalStartMinutes = startHour * 60 + startMinute;
+  const totalEndMinutes = endHour * 60 + endMinute;
+  const minutesDiff = totalEndMinutes - totalStartMinutes;
+  
+  // 1시간 당 픽셀로 계산하여 반환 (예: 30분에 40px)
+  const pixelPerHour = 80;
+  const heightInPixel = (minutesDiff / 60) * pixelPerHour;
+
+  return `${heightInPixel}px`;
 };
 
 const DayView: React.FC<Props> = ({
@@ -63,22 +83,32 @@ const DayView: React.FC<Props> = ({
         {hours.map((hour) => (
           <div key={hour} className="outer-box w-[1024px] h-[80px]"></div>
         ))}
+
+
+
+        
       </div>
       
 
       
      <div className='absolute top-3 left-3'>
     {hours.map((hour) => (
-      <div key={hour} className="flex flex-col w-[1024px] h-[80px]">
+      <div key={hour} className="flex flex-col w-[1024px] ">
         {filterDate(todoData, today, hour).map((item: Todo, index: number) => (
           <div
             key={index}
-            className={clsx('flex flex-row gap-4', {
+            className={clsx('event-box','flex flex-row gap-4', {
               'bg-primary text-white': hour === today.getHours(),
             })}
+            style={{
+              top: calculateTopPosition(item.startTime), 
+              height: calculateHeight(item.startTime, item.endTime),
+            }}
           >
+             <div className="bg-green-500">            
             <div>{item.title}</div>
-            <div>{item.startTime}</div>
+            <div>{item.startTime}-{item.endTime}</div>
+          </div>
           </div>
         ))}
 
