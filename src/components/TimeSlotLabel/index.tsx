e0e290bf9b033bcca940539e51877db7a629302a
hanchi <hanchi@tosky.co.kr>
2024-01-02 10:03:24 +0900

    feat: 타임 슬롯 추가

---
+++ src/components/TimeSlotLabel/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {};
+
+export default function TimeSlotLabel({}: Props) {
+  return (
+    <div className="flex flex-col">
+      {Array.from({ length: 24 }, (_, i) => i).map((hour) => (
+        <div key={hour} className="h-[60px] w-[70px] relative">
+          {hour > 0 && (
+            <div className="absolute -top-3.5 right-1">{`${
+              hour >= 12 ? "오후" : "오전"
+            } ${hour >= 12 ? hour - 12 : hour}시`}</div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
