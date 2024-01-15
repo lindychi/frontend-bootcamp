04efacef65ec2fdc35cf91cc79f0e453fb157e69
KKH1992 <goldhyuk92@gmail.com>
2024-01-15 18:09:24 +0900

    달력수정중

--- src/components/MonthView.tsx
+++ src/components/MonthView.tsx
@@ -1,47 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { dayList } from "../consts/calendar";
-
-type EventType = {
-  date: string;
-  name: string;
-  // time: string;
-  // endTime: string;
-};
+import { getEvents } from "../services/eventService";
+import { EventItem } from "../types/common";
 
 type BigCalendarProps = {
   targetCalendarDates: Date[] | null;
   getSecondDateClass: (date: Date) => string;
-  //  events: EventType[];
 };
 
 const BigCalendar: React.FC<BigCalendarProps> = ({
   targetCalendarDates,
   getSecondDateClass,
-  // events,
 }) => {
-  // const groupAndSortEvents = () => {
-  //   const groupedEvents: { [key: string]: EventType[] } = {};
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth() + 1;
 
-  // events.forEach((event) => {
-  //   const dateKey = new Date(event.date).toLocaleDateString("ko-KR"); // Modified this line
-  //   if (!groupedEvents[dateKey]) {
-  //     groupedEvents[dateKey] = [];
-  //   }
-  //   groupedEvents[dateKey].push(event);
-  // });
+  const [events, setEvents] = useState<EventItem[]>([]);
+  const [loading, setloading] = useState<boolean>(true);
 
-  // for (const key in groupedEvents) {
-  //   groupedEvents[key].sort((a, b) => {
-  //     const timeA = new Date(`1970-01-01T${a.time}`);
-  //     const timeB = new Date(`1970-01-01T${b.time}`);
-  //     return timeA.getTime() - timeB.getTime();
-  //   });
-  // }
+  const loadEvents = async () => {
+    try {
+      const result = await getEvents({
+        year: currentYear,
+        month: currentMonth,
+      });
+      setEvents(result.data);
+    } finally {
+      setloading(false);
+    }
+  };
+  useEffect(() => {
+    loadEvents();
+  }, []);
 
-  //   return groupedEvents;
-  // };
+  const groupAndSortEvents = () => {
+    const groupedEvents: { [key: string]: EventItem[] } = {};
 
-  // const groupedAndSortedEvents = groupAndSortEvents();
+    events.forEach((event) => {
+      const dateKey = new Date(event.startedAt).toLocaleDateString("ko-KR"); // 변경된 부분
+      if (!groupedEvents[dateKey]) {
+        groupedEvents[dateKey] = [];
+      }
+      groupedEvents[dateKey].push(event);
+    });
+
+    for (const key in groupedEvents) {
+      groupedEvents[key].sort((a, b) => {
+        const timeA = new Date(a.startedAt);
+        const timeB = new Date(b.startedAt);
+        return timeA.getTime() - timeB.getTime();
+      });
+    }
+
+    return groupedEvents;
+  };
+
+  const groupedAndSortedEvents = groupAndSortEvents();
 
   return (
     <>
@@ -54,29 +69,31 @@ const BigCalendar: React.FC<BigCalendarProps> = ({
       </div>
 
       <div className="min-w-screen min-h-[1500px] grid grid-cols-7  border border-state-300">
-        {targetCalendarDates?.map((date: Date, index: number) => {
-          // const currentDateKey = date.toLocaleDateString("ko-KR");
-          // const currentDateEvents =
-          //   groupedAndSortedEvents[currentDateKey] || [];
+        {loading ? (
+          <div>로딩중</div>
+        ) : (
+          targetCalendarDates?.map((date: Date, index: number) => {
+            const currentDateKey = date.toLocaleDateString("ko-KR");
+            const currentDateEvents =
+              groupedAndSortedEvents[currentDateKey] || [];
 
-          // console.info(date.getDate());
-
-          return (
-            <div
-              key={index}
-              className={`text-left indent-3 py-2 border border-state-300 ${getSecondDateClass(
-                date
-              )}`}
-            >
-              {date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`}
-              {/* {currentDateEvents.map((event: EventType, eventIndex: number) => (
-                <div key={eventIndex}>
-                  {event.name} at {event.time}
-                </div>
-              ))} */}
-            </div>
-          );
-        })}
+            return (
+              <div
+                key={index}
+                className={`text-left indent-3 py-2 border border-state-300 truncate w=[20px]${getSecondDateClass(
+                  date
+                )}`}
+              >
+                {date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`}
+                {currentDateEvents.map(
+                  (event: EventItem, eventIndex: number) => (
+                    <div key={eventIndex}>{event.title}</div>
+                  )
+                )}
+              </div>
+            );
+          })
+        )}
       </div>
     </>
   );
